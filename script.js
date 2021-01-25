cd2e7766f004d98aed1ae366f99faa777c902b92
mralimov <mralimov89@gmail.com>
2021-01-24 20:27:44 -0500

    Destructuring movement dates & convert to days JS

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-11-27T17:01:17.194Z',
-    '2020-12-01T23:36:17.929Z',
-    '2020-12-02T10:51:36.790Z',
+    '2021-01-19T17:01:17.194Z',
+    '2021-01-23T23:36:17.929Z',
+    '2021-01-25T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT',
@@ -109,6 +109,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//Date/Time/Days function destructutring
+const formatMovementDate = date => {
+  //Finding how many days passed/
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+};
+
 const displayMovements = (account, sort = false) => {
   //TextContent = 0
   containerMovements.innerHTML = '';
@@ -122,14 +141,8 @@ const displayMovements = (account, sort = false) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(account.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
 
-    //Finding how many days passed/
-    const calcDaysPassed = (date1, date2) =>
-      Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+    const displayDate = formatMovementDate(date);
 
     const html = `
     <div class="movements__row">
