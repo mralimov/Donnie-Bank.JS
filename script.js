c94caa3ca9255277712996f47b059d7767abe940
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-02-18 15:47:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Donnie Alimov',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
